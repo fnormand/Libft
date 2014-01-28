1b954211b58845cc5a8a8db0b1e7963db002cff2
NORMAND Franck <fnormand@student.42.fr>
2014-01-28 18:54:52 +0100

    Update libft.h

--- libft.h
+++ libft.h
@@ -17,14 +17,14 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);/*return need to be free()if possible*/
 char	*ft_strcpy(char *s1, const char *s2);
-char    *ft_strncpy(char *s1, const char *s2, size_t n);
+char	*ft_strncpy(char *s1, const char *s2, size_t n);
 char	*ft_strcat(char *s1, const char *s2);
 char	*ft_strncat(char *s1, const char *s2, size_t n);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strstr(const char *s1, const char *s2);
-char    *ft_strnstr(const char *s1, const char *s2, size_t n);
+char	*ft_strnstr(const char *s1, const char *s2, size_t n);
 int	ft_strcmp(const char *s1, const char *s2);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
 int	ft_atoi(const char *str);
